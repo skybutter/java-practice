70a0befc71fe616bb586d5fa7bd07b9345557739
alan.wong <alan.wong@weboctave.com>
2022-02-02 20:24:51 -0800

    remove unused import

--- src/main/java/alan/FindBlockLeastDistance.java
+++ src/main/java/alan/FindBlockLeastDistance.java
@@ -2,7 +2,6 @@ package alan;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.*;
